adcb7d5fbf9e6ae12399420a677d242adf9c5a41
Marianne <mariannemukai@gmail.com>
2025-09-29 11:34:01 -0300

    Declarando JavaScript

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+//alert ("Hello world")
+
+//DECLARACAO E VARIAVEIS:
+//var=declaração  nome=variavel  
+
+var nome = "Fiap";
+console.log(typeof nome)
+
+//let:outra forma de declarara, mesma função do var
+let nome1 = 10;
+console.log(typeof nome1);
+
+//const: constante. Significa que o "exemplo1", nao pode ser alterado
+const nome2 = {};
+console.log(typeof nome2)
+let nome3 = []
+console.log(typeof nome3)
+
+//typeof: diz o nome do tipo da variavel
+let nome4=true;
+console.log(typeof nome4)
+
+
+// {}=Objeto->lista
+// []=Array->conjunto de objetos->lista
+// ()=Metodo->terá parametros
